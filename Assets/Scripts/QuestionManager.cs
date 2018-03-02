2cd7faf7ea4111120e7343a86ce5c5280a35c6c8
Yasuhiro <yasuhiro1026a@hotmail.com>
2018-03-02 18:07:13 +0900

    add unirx(not use yet), and add Question prefab

---
+++ Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class QuestionManager : MonoBehaviour
+{
+    // 問題のプレハブ
+    public GameObject QuestionPrefab;
+
+    private GameObject[] QuestionArray = new GameObject[4];
+
+    public Animator QuestionAnimator;
+
+    private TimeManager _timeMan = new TimeManager();
+
+    // Use this for initialization
+    void Start()
+    {
+        _timeMan.TimeElapsed += (s, e) =>
+        {
+            // プレハブ化
+            // arrayの３が一番遠いオブジェクト 
+            QuestionArray[3] = Instantiate(QuestionPrefab, QuestionPrefab.transform.position, QuestionPrefab.transform.rotation);
+
+            // 一つ進める
+            QuestionsNextMove();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void QuestionsNextMove()
+    {
+        // 各問題を次に進める
+        GameObject.Destroy(QuestionArray[0], 1.0f);
+        QuestionArray[0] = QuestionArray[1];
+        QuestionArray[1] = QuestionArray[2];
+        QuestionArray[2] = QuestionArray[3];
+        QuestionArray[3] = null;
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+public class TimeManager
+{
+    // gameの単位時間（出題感覚）
+    public float GameInterval { get; set; } = 1.0f;
+
+    public bool Loop { get; set; } = false;
+
+    private Timer _timer = new Timer();
+
+	public event EventHandler TimeElapsed;
+
+    public TimeManager()
+    {
+		_timer.Elapsed += (s,e) =>
+		{
+			TimeElapsed?.Invoke(this, EventArgs.Empty);
+
+			if(Loop)
+				_timer.Start();
+		};
+    }
+
+    // イベント
+    public void TimerStart()
+    {
+        _timer.Start();
+    }
+
+    public void TimeStop()
+    {
+        _timer.Stop();
+    }
+}
